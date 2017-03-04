137b4724b53f79b5250a5f62fa644697018d9dcf
jesseharold <jesseharold@gmail.com>
2017-03-03 16:10:37 -0800

    building out file structure and react components

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+// tell the app to look for static files in these directories
+app.use(express.static('./server/static/'));
+app.use(express.static('./client/dist/'));
+
+// start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
